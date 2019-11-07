d91f4daacf1b279e2224a50eba15deae24e3d175
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-11-07 17:59:53 -0600

    Added planetData, HeroImage, and Header.

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import '../HeroImage/HeroImage';
+import HeroImage from '../HeroImage/HeroImage';
+
+const Header = () => {
+  return (
+    <header>
+      <nav className='header-nav'>
+        <a className='nav-link' href='#'>Home</a>
+        <a className='nav-link' href='#'>About</a>
+        <a className='nav-link' href='#'>Blog</a>
+        <a className='nav-link' href='#'>Contact</a>
+      </nav>
+      <HeroImage />
+    </header>
+  )
+}
+
+export default Header;
