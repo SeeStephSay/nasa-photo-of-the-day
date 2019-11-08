4939115166bbefc89cbc57c9d73fdadbd4810da1
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-11-07 22:28:26 -0600

    Edited HeroImage file.

--- src/Components/HeroImage/HeroImage.js
+++ src/Components/HeroImage/HeroImage.js
@@ -1,26 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const HeroImage = () {
-  const [apod, setApod] = useState('');
-  const endpoint = "https://api.nasa.gov/planetary/apod?api_key=9c73Q6Mf7hA7aSdegCd85R4aYTyLPhAeupzLdSom";
+const HeroImage = () => {
+	const [ apod, setApod ] = useState('');
+	const endpoint = 'https://api.nasa.gov/planetary/apod?api_key=9c73Q6Mf7hA7aSdegCd85R4aYTyLPhAeupzLdSom';
 
-  useEffect(() = {
-    axios
-      .get(endpoint)
-      .then(response => {
-        const data = response.data;
-        setApod(data);
-      })
-      .catch(error => console.log("Error occurred during API call.", error))
-  }, []);
+	useEffect(() => {
+		axios
+			.get(endpoint)
+			.then((response) => {
+				const data = response.data;
+				setApod(data);
+			})
+			.catch((error) => console.log('Error occurred during API call.', error));
+	}, []);
 
-  return (
-    <div className='heroImgContainer'>
-      <img className='heroImg' src={apod.hdurl} alt={apod.title} />
-      <p className='apodDesc'>{apod.explanation}</p>
-    </div>
-  )
-}
+	return (
+		<div className='heroImgContainer'>
+			<img className='heroImg' src={apod.hdurl} alt={apod.title} />
+			<p className='apodDesc'>{apod.explanation}</p>
+		</div>
+	);
+};
 
 export default HeroImage;
